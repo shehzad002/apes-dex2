07164ed3537c10340dd7220840675c6bf8f18703
shehzad002 <shehzad.khan145@yahoo.com>
2021-05-07 11:36:40 -0700

    shehzad added dex

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,58 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '#',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '#',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '#',
+  },
+  
+
+ 
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+    
+      {
+        label: 'Github',
+        href: '#',
+      },
+      {
+        label: 'Docs',
+        href: '#',
+      },
+      {
+        label: 'Blog',
+        href: '#',
+      },
+    ],
+  },
+]
+
+export default config
